2aebde89dae1ef710dd9de92423f7d35c4858be5
itool <itbaby@users.noreply.github.com>
2025-06-03 20:03:39 +0800

    update base on changes

--- src/App.svelte
+++ src/App.svelte
@@ -2,15 +2,17 @@
   import "./app.css";
   import { switchLanguage } from "./i18n";
   import { isLoading, _ } from "svelte-i18n";
-  import IMenu from './IMenu.svelte';
+  import IMenu from "./IMenu.svelte";
   import IFooter from "./IFooter.svelte";
   import ICarousel from "./ICarousel.svelte";
+  import IScroller from "./IScroller.svelte";
 </script>
 
 {#if $isLoading}{:else}
   <div class="flex flex-col">
-    <IMenu/>
-    <IFooter/>
+    <IMenu />
+    <IScroller />
+    <IFooter />
   </div>
 {/if}
 
