62feae84702d5376690922f9f24b95be362c36c4
itool <itbaby@users.noreply.github.com>
2025-08-02 18:30:46 +0800

    menu done

--- src/features/CMenu.svelte
+++ src/features/CMenu.svelte
@@ -267,7 +267,7 @@
               ? 'block'
               : 'hidden'}"
           >
-            <ul class="py-2 text-sm md:py-2">
+            <ul class="py-2 text-sm md:py-2 whitespace-nowrap">
               <li>
                 <a href="#" class="block px-8 py-2 hover:bg-gray-800 md:px-4"
                   >{$_("menu.headhunting")}</a
@@ -314,7 +314,7 @@
               ? 'block'
               : 'hidden'}"
           >
-            <ul class="py-2 text-sm md:py-2">
+            <ul class="py-2 text-sm md:py-2 whitespace-nowrap">
               <li>
                 <a href="#" class="block px-8 py-2 hover:bg-gray-800 md:px-4"
                   >{$_("menu.marketAnalysis")}</a
@@ -357,7 +357,7 @@
               ? 'block'
               : 'hidden'}"
           >
-            <ul class="py-2 text-sm md:py-2">
+            <ul class="py-2 text-sm md:py-2 whitespace-nowrap">
               <li>
                 <a href="#" class="block px-8 py-2 hover:bg-gray-800 md:px-4"
                   >{$_("menu.companyIntroduction")}</a
