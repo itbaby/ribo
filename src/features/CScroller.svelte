8e9ce28411c5cad77aca893730ac8ea09fc7c906
itool <itbaby@users.noreply.github.com>
2025-08-02 17:08:54 +0800

    update icon

--- src/features/CScroller.svelte
+++ src/features/CScroller.svelte
@@ -1,63 +1,75 @@
+<script>
+  import alibaba from '../assets/icons-trans/alibaba.png';
+  import alo from '../assets/icons-trans/alo.png';
+  import cisco from '../assets/icons-trans/cisco.png';
+  import colt from '../assets/icons-trans/colt.png';
+  import fcn from '../assets/icons-trans/fcn.png';
+  import fil from '../assets/icons-trans/fil.png';
+  import hitachi from '../assets/icons-trans/hitachi.png';
+  import lenovo from '../assets/icons-trans/lenovo.png';
+  import tcl from '../assets/icons-trans/tcl.png';
+</script>
+
 <div class="wrapper">
   <div class="carousel">
     <div class="carousel__item">
-      <div class="carousel__item-head">🐳</div>
+      <div class="carousel__item-head"><img src={alibaba} alt="alibaba" /></div>
       <div class="carousel__item-body">
         <p class="title">spouting whale</p>
         <p>Unicode: U+1F433</p>
       </div>
     </div>
     <div class="carousel__item">
-      <div class="carousel__item-head">🐋</div>
+      <div class="carousel__item-head"><img src={alo} alt="alo" /></div>
       <div class="carousel__item-body">
         <p class="title">whale</p>
         <p>Unicode: U+1F40B</p>
       </div>
     </div>
     <div class="carousel__item">
-      <div class="carousel__item-head">🐬</div>
+      <div class="carousel__item-head"><img src={cisco} alt="cisco" /></div>
       <div class="carousel__item-body">
         <p class="title">dolphin</p>
         <p>Unicode: U+1F42C</p>
       </div>
     </div>
     <div class="carousel__item">
-      <div class="carousel__item-head">🐟</div>
+      <div class="carousel__item-head"><img src={colt} alt="colt" /></div>
       <div class="carousel__item-body">
         <p class="title">fish</p>
         <p>Unicode: U+1F41F</p>
       </div>
     </div>
     <div class="carousel__item">
-      <div class="carousel__item-head">🐠</div>
+      <div class="carousel__item-head"><img src={fcn} alt="fcn" /></div>
       <div class="carousel__item-body">
         <p class="title">tropical fish</p>
         <p>Unicode: U+1F420</p>
       </div>
     </div>
     <div class="carousel__item">
-      <div class="carousel__item-head">🐡</div>
+      <div class="carousel__item-head"><img src={fil} alt="fil" /></div>
       <div class="carousel__item-body">
         <p class="title">blowfish</p>
         <p>Unicode: U+1F421</p>
       </div>
     </div>
     <div class="carousel__item">
-      <div class="carousel__item-head">🦈</div>
+      <div class="carousel__item-head"><img src={hitachi} alt="hitachi" /></div>
       <div class="carousel__item-body">
         <p class="title">shark</p>
         <p>Unicode: U+1F988</p>
       </div>
     </div>
     <div class="carousel__item">
-      <div class="carousel__item-head">🐙</div>
+      <div class="carousel__item-head"><img src={lenovo} alt="lenovo" /></div>
       <div class="carousel__item-body">
         <p class="title">octopus</p>
         <p>Unicode: U+1F419</p>
       </div>
     </div>
     <div class="carousel__item">
-      <div class="carousel__item-head">🐚</div>
+      <div class="carousel__item-head"><img src={tcl} alt="tcl" /></div>
       <div class="carousel__item-body">
         <p class="title">spiral shell</p>
         <p>Unicode: U+1F41A</p>
@@ -114,7 +126,7 @@
 
   .carousel__item-head {
     border-radius: 50%;
-    background-color:var(--color-gray-900);
+    background-color:var(--color-white);
     width: 90px;
     height: 90px;
     padding: 14px;
@@ -129,7 +141,7 @@
 
   .carousel__item-body {
     width: 100%;
-    background-color: var(--color-gray-400);
+    background-color: var(--color-gray-200);
     border-radius: 8px;
     padding: 16px 20px 16px 70px;
   }
@@ -146,25 +158,25 @@
       opacity: 0;
       visibility: hidden;
     }
-    #{$slide-change-timing}%,
-    #{$animation-steps-fraction}% {
+    3%,
+    11.11% {
       transform: translateY(100%) scale(0.7);
       opacity: 0.4;
       visibility: visible;
     }
-    #{$animation-steps-fraction + $slide-change-timing}%,
-    #{$animation-steps-fraction * 2}% {
+    14.11%,
+    22.22% {
       transform: translateY(0) scale(1);
       opacity: 1;
       visibility: visible;
     }
-    #{($animation-steps-fraction * 2) + $slide-change-timing}%,
-    #{$animation-steps-fraction * 3}% {
+    25.22%,
+    33.33% {
       transform: translateY(-100%) scale(0.7);
       opacity: 0.4;
       visibility: visible;
     }
-    #{($animation-steps-fraction * 3) + $slide-change-timing}% {
+    36.33% {
       transform: translateY(-100%) scale(0.5);
       opacity: 0;
       visibility: visible;
