c5c78cc48fb6f672408cb398511e8f7f40c5947f
itool <itbaby@users.noreply.github.com>
2025-08-15 11:42:04 +0800

    feat(ui): add features and testimonials sections to tech category

--- src/features/CTechCategory.svelte
+++ src/features/CTechCategory.svelte
@@ -60,6 +60,63 @@
 
 <!-- Features -->
  <!-- svelte-ignore a11y_img_redundant_alt -->
+<div class="bg-gray-900 py-24 sm:py-32">
+  <div class="mx-auto max-w-7xl px-6 lg:px-8">
+    <div class="mx-auto max-w-2xl lg:text-center">
+      <h2 class="text-base/7 font-semibold text-indigo-400">Deploy faster</h2>
+      <p class="mt-2 text-4xl font-semibold tracking-tight text-pretty text-white sm:text-5xl lg:text-balance">Everything you need to deploy your app</p>
+      <p class="mt-6 text-lg/8 text-gray-300">Quis tellus eget adipiscing convallis sit sit eget aliquet quis. Suspendisse eget egestas a elementum pulvinar et feugiat blandit at. In mi viverra elit nunc.</p>
+    </div>
+    <div class="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
+      <dl class="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
+        <div class="relative pl-16">
+          <dt class="text-base/7 font-semibold text-white">
+            <div class="absolute top-0 left-0 flex size-10 items-center justify-center rounded-lg bg-indigo-500">
+              <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" data-slot="icon" aria-hidden="true" class="size-6 text-white">
+                <path d="M12 16.5V9.75m0 0 3 3m-3-3-3 3M6.75 19.5a4.5 4.5 0 0 1-1.41-8.775 5.25 5.25 0 0 1 10.233-2.33 3 3 0 0 1 3.758 3.848A3.752 3.752 0 0 1 18 19.5H6.75Z" stroke-linecap="round" stroke-linejoin="round" />
+              </svg>
+            </div>
+            Push to deploy
+          </dt>
+          <dd class="mt-2 text-base/7 text-gray-400">Morbi viverra dui mi arcu sed. Tellus semper adipiscing suspendisse semper morbi. Odio urna massa nunc massa.</dd>
+        </div>
+        <div class="relative pl-16">
+          <dt class="text-base/7 font-semibold text-white">
+            <div class="absolute top-0 left-0 flex size-10 items-center justify-center rounded-lg bg-indigo-500">
+              <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" data-slot="icon" aria-hidden="true" class="size-6 text-white">
+                <path d="M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z" stroke-linecap="round" stroke-linejoin="round" />
+              </svg>
+            </div>
+            SSL certificates
+          </dt>
+          <dd class="mt-2 text-base/7 text-gray-400">Sit quis amet rutrum tellus ullamcorper ultricies libero dolor eget. Sem sodales gravida quam turpis enim lacus amet.</dd>
+        </div>
+        <div class="relative pl-16">
+          <dt class="text-base/7 font-semibold text-white">
+            <div class="absolute top-0 left-0 flex size-10 items-center justify-center rounded-lg bg-indigo-500">
+              <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" data-slot="icon" aria-hidden="true" class="size-6 text-white">
+                <path d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99" stroke-linecap="round" stroke-linejoin="round" />
+              </svg>
+            </div>
+            Simple queues
+          </dt>
+          <dd class="mt-2 text-base/7 text-gray-400">Quisque est vel vulputate cursus. Risus proin diam nunc commodo. Lobortis auctor congue commodo diam neque.</dd>
+        </div>
+        <div class="relative pl-16">
+          <dt class="text-base/7 font-semibold text-white">
+            <div class="absolute top-0 left-0 flex size-10 items-center justify-center rounded-lg bg-indigo-500">
+              <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" data-slot="icon" aria-hidden="true" class="size-6 text-white">
+                <path d="M7.864 4.243A7.5 7.5 0 0 1 19.5 10.5c0 2.92-.556 5.709-1.568 8.268M5.742 6.364A7.465 7.465 0 0 0 4.5 10.5a7.464 7.464 0 0 1-1.15 3.993m1.989 3.559A11.209 11.209 0 0 0 8.25 10.5a3.75 3.75 0 1 1 7.5 0c0 .527-.021 1.049-.064 1.565M12 10.5a14.94 14.94 0 0 1-3.6 9.75m6.633-4.596a18.666 18.666 0 0 1-2.485 5.33" stroke-linecap="round" stroke-linejoin="round" />
+              </svg>
+            </div>
+            Advanced security
+          </dt>
+          <dd class="mt-2 text-base/7 text-gray-400">Arcu egestas dolor vel iaculis in ipsum mauris. Tincidunt mattis aliquet hac quis. Id hac maecenas ac donec pharetra eget.</dd>
+        </div>
+      </dl>
+    </div>
+  </div>
+</div>
 <div class="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
 	<div class="relative p-6 md:p-16">
 		<!-- Grid -->
@@ -192,4 +249,101 @@
 		<!-- End Background Color -->
 	</div>
 </div>
-<!-- End Features -->
+
+
+
+<!-- Testimonials with Stats -->
+<div class="max-w-[82rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
+  <!-- Grid -->
+  <div class="lg:grid lg:grid-cols-12 lg:gap-16 lg:items-center lg:justify-between">
+    <div class="lg:col-span-5 lg:col-start-1">
+      <!-- Title -->
+      <div class="mb-8">
+        <h2 class="mb-2 text-3xl text-white font-bold lg:text-4xl dark:text-neutral-200">
+          It's all about speed
+        </h2>
+        <p class="text-white dark:text-neutral-400">
+          We provide you with a test account that can be set up in seconds. Our main focus is getting responses to you as soon as we can.
+        </p>
+      </div>
+      <!-- End Title -->
+
+      <!-- Blockquote -->
+      <blockquote class="relative">
+        <svg class="absolute top-0 start-0 transform -translate-x-6 -translate-y-8 size-16 text-gray-200 dark:text-neutral-800" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+          <path d="M7.39762 10.3C7.39762 11.0733 7.14888 11.7 6.6514 12.18C6.15392 12.6333 5.52552 12.86 4.76621 12.86C3.84979 12.86 3.09047 12.5533 2.48825 11.94C1.91222 11.3266 1.62421 10.4467 1.62421 9.29999C1.62421 8.07332 1.96459 6.87332 2.64535 5.69999C3.35231 4.49999 4.33418 3.55332 5.59098 2.85999L6.4943 4.25999C5.81354 4.73999 5.26369 5.27332 4.84476 5.85999C4.45201 6.44666 4.19017 7.12666 4.05926 7.89999C4.29491 7.79332 4.56983 7.73999 4.88403 7.73999C5.61716 7.73999 6.21938 7.97999 6.69067 8.45999C7.16197 8.93999 7.39762 9.55333 7.39762 10.3ZM14.6242 10.3C14.6242 11.0733 14.3755 11.7 13.878 12.18C13.3805 12.6333 12.7521 12.86 11.9928 12.86C11.0764 12.86 10.3171 12.5533 9.71484 11.94C9.13881 11.3266 8.85079 10.4467 8.85079 9.29999C8.85079 8.07332 9.19117 6.87332 9.87194 5.69999C10.5789 4.49999 11.5608 3.55332 12.8176 2.85999L13.7209 4.25999C13.0401 4.73999 12.4903 5.27332 12.0713 5.85999C11.6786 6.44666 11.4168 7.12666 11.2858 7.89999C11.5215 7.79332 11.7964 7.73999 12.1106 7.73999C12.8437 7.73999 13.446 7.97999 13.9173 8.45999C14.3886 8.93999 14.6242 9.55333 14.6242 10.3Z" fill="currentColor"/>
+        </svg>
+
+        <div class="relative z-10">
+          <p class="text-xl italic text-white dark:text-white">
+            Amazing people to work with. Very fast and professional partner.
+          </p>
+        </div>
+
+        <footer class="mt-6">
+          <div class="flex items-center gap-x-4">
+            <div class="shrink-0">
+              <img class="size-8 rounded-full" src="https://images.unsplash.com/photo-1492562080023-ab3db95bfbce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=320&h=320&q=80" alt="Avatar">
+            </div>
+            <div class="grow">
+              <div class="font-semibold text-white dark:text-neutral-200">Josh Grazioso</div>
+              <div class="text-xs text-white dark:text-neutral-500">Director Payments & Risk | Airbnb</div>
+            </div>
+          </div>
+        </footer>
+      </blockquote>
+      <!-- End Blockquote -->
+    </div>
+    <!-- End Col -->
+
+    <div class="mt-10 lg:mt-0 lg:col-span-6 lg:col-end-13">
+      <div class="space-y-6 sm:space-y-8">
+        <!-- List -->
+        <ul class="grid grid-cols-2 divide-y divide-y-2 divide-x divide-x-2 divide-gray-200 overflow-hidden dark:divide-neutral-700">
+          <li class="flex items-center -m-0.5 p-4 sm:p-8">
+            <div class="flex items-end gap-x-2 text-3xl sm:text-5xl font-bold text-white dark:text-neutral-200">
+              45k+
+            </div>
+            <p class="text-sm sm:text-base text-white dark:text-neutral-400 ml-4">
+              users - from new startups to public companies
+            </p>
+          </li>
+
+          <li class="flex items-center -m-0.5 p-4 sm:p-8">
+            <div class="flex items-end gap-x-2 text-3xl sm:text-5xl font-bold text-white dark:text-neutral-200">
+              <svg class="shrink-0 size-5 text-blue-600 dark:text-blue-500" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m5 12 7-7 7 7"/><path d="M12 19V5"/></svg>
+              23%
+            </div>
+            <p class="text-sm sm:text-base text-white dark:text-neutral-400 ml-4">
+              increase in traffic on webpages with Looms
+            </p>
+          </li>
+
+          <li class="flex items-center -m-0.5 p-4 sm:p-8">
+            <div class="flex items-end gap-x-2 text-3xl sm:text-5xl font-bold text-white dark:text-neutral-200">
+              <svg class="shrink-0 size-5 text-blue-600 dark:text-blue-500" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m5 12 7-7 7 7"/><path d="M12 19V5"/></svg>
+              9.3%
+            </div>
+            <p class="text-sm sm:text-base text-white dark:text-neutral-400 ml-4">
+              boost in reply rates across sales outreach
+            </p>
+          </li>
+
+          <li class="flex items-center -m-0.5 p-4 sm:p-8">
+            <div class="flex items-end gap-x-2 text-3xl sm:text-5xl font-bold text-white dark:text-neutral-200">
+              2x
+            </div>
+            <p class="text-sm sm:text-base text-white dark:text-neutral-400 ml-4">
+              faster than previous Preline versions
+            </p>
+          </li>
+        </ul>
+        <!-- End List -->
+      </div>
+    </div>
+    <!-- End Col -->
+  </div>
+  <!-- End Grid -->
+</div>
+<!-- End Testimonials with Stats -->
+<!-- End Card Blog -->
