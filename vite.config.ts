917ba1a24fa9e11afbd3524dfefe7630deaaf621
itool <itbaby@users.noreply.github.com>
2025-08-16 19:47:41 +0800

    build(compiler): update target to ES2022 across configs

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,17 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 import { visualizer } from 'rollup-plugin-visualizer';
 
 export default defineConfig({
+    esbuild: {
+        target: 'es2022'
+    },
     plugins: [tailwindcss(), svelte(), visualizer({
         open: true,
         filename: 'dist/stats.html'
     })],
     build: {
         chunkSizeWarningLimit: 1000,
-        cssCodeSplit: true
+        cssCodeSplit: true,
+        target: 'es2022'
     },
     optimizeDeps: {
         include: ['aos'],
