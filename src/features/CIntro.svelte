a0a3a5041ab93857a05ff421e45199aa2764ecca
itool <itbaby@users.noreply.github.com>
2025-08-23 09:31:43 +0800

    style(intro): update background color to darker shade

--- src/features/CIntro.svelte
+++ src/features/CIntro.svelte
@@ -76,7 +76,7 @@
   });
 </script>
 
-<div class="bg-gray-800 py-24 sm:py-32">
+<div class="bg-gray-900 py-24 sm:py-32">
   <div class="mx-auto max-w-7xl px-6 lg:px-8">
       <div class="mx-auto max-w-7xl px-6 lg:px-8">
     <div class="text-center">
